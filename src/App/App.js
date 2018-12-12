f2589643d07e99948b82b5cf9b03696d10db7b9b
mohammad <khezri@outlook.com>
2018-12-11 20:52:10 -0600

    added auth state

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import connection from '../helpers/data/connection';
-
 import Auth from '../components/Auth/Auth';
 import './App.scss';
+import Listings from '../components/Listings/Listings';
 
 class App extends Component {
+
+  state = {
+    authed: false,
+  }
 
   componentDidMount() {
     connection();
   }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true })
+  }
+
   render() {
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      )
+    }
+
+
     return (
       <div className="App">
         <Auth />
-      </div >
+        <Listings />
+      </div>
     );
   }
 }
-
 export default App;
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import authRequests from '../../helpers/data/authRequest';
 import './Auth.scss';
 
-
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault(); // Stop all events and just do this event
+    authRequests.authenticate().then(() => {
+      this.props.isAuthenticated();
+    }).catch(err => console.error('There was an error', err));
+  }
+
+
   render() {
     return (
       <div className="Auth">
-        <button className="btn btn-danger">Login</button>
+        <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
       </div>
     );
   }
