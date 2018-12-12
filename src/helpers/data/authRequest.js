31ca11cb8899260d8667702bb438d717c068df54
mohammad <khezri@outlook.com>
2018-12-11 23:10:43 -0600

    Authed

--- src/helpers/data/authRequest.js
+++ src/helpers/data/authRequest.js
@@ -5,5 +5,6 @@ const authenticate = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   return firebase.auth().signInWithPopup(provider);
 };
+const logoutUser = () => firebase.auth().signOut();
 
-export default { authenticate };
+export default { authenticate, logoutUser };
